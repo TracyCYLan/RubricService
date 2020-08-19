94234f5187d7fe7f19eb933d3d3448db55380c3b
TracyCYLan <g0170po@gmail.com>
2020-08-19 19:54:38 +0800

    download attachment to local

--- src/main/java/edu/csula/rubrics/web/AssessmentController.java
+++ src/main/java/edu/csula/rubrics/web/AssessmentController.java
@@ -1,5 +1,7 @@
 package edu.csula.rubrics.web;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -7,13 +9,25 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Properties;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -100,23 +114,47 @@ public class AssessmentController {
 		return groups;
 	}
 
-	// view or download file ----
+	// view file ----
 	@GetMapping("/artifact/{id}")
 	public String readFile(@PathVariable Long id) throws IOException {
 		Artifact artifact = artifactDao.getArtifact(id);
-		String path = readProp("canvas.downloadpath") + artifact.getPath() + "\\" + artifact.getName();
-		//read file and return text to web page
+		String fileName = artifact.getName();
+		String path = readProp("canvas.downloadpath") + artifact.getPath() + "\\" + fileName;
+		// read file and return text to web page
 		File file = new File(path);
 		BufferedReader br = new BufferedReader(new FileReader(file));
 		StringBuilder sb = new StringBuilder();
 		String s;
-		while ((s = br.readLine()) != null)
-		{
+		while ((s = br.readLine()) != null) {
 			sb.append(s);
 			sb.append("\n");
 		}
-		
+
 		return sb.toString();
+	}
+
+	// download file to user local ---
+	@GetMapping("/artifact/{id}/download")
+	public ResponseEntity<byte[] > download(@PathVariable Long id) throws IOException {
+		Artifact artifact = artifactDao.getArtifact(id);
+		String fileName = artifact.getName();
+		String filePath = readProp("canvas.downloadpath") + artifact.getPath() + "\\" + fileName;
+		String contentType = artifact.getContentType();
+
+		File file = new File(filePath);
+//--------
+		
+		Path path = Paths.get(filePath);
+		byte[] data = Files.readAllBytes(path);
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentDispositionFormData("attachment", fileName);
+		headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+		return new ResponseEntity<>(data, headers, HttpStatus.OK);
+//		ByteArrayResource resource = new ByteArrayResource(data);
+//
+//		return ResponseEntity.ok()
+//				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + path.getFileName().toString())
+//				.contentType(MediaType.parseMediaType(contentType)).contentLength(data.length).body(resource);
 	}
 //	// create an evaluation. Do we need to think about how to deal with Task status?
 //	/*
