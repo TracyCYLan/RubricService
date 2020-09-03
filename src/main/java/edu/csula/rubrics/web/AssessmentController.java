b50c000021ee0caef4c2f4c948420385b3710db0
TracyCYLan <g0170po@gmail.com>
2020-09-03 12:13:23 +0800

    remove unused imports

--- src/main/java/edu/csula/rubrics/web/AssessmentController.java
+++ src/main/java/edu/csula/rubrics/web/AssessmentController.java
@@ -5,49 +5,30 @@ import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 import java.util.Properties;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import edu.csula.rubrics.models.Artifact;
 import edu.csula.rubrics.models.Assessment;
 import edu.csula.rubrics.models.AssessmentGroup;
-import edu.csula.rubrics.models.Rubric;
-import edu.csula.rubrics.models.User;
 import edu.csula.rubrics.models.dao.ArtifactDao;
 import edu.csula.rubrics.models.dao.AssessmentDao;
-import edu.csula.rubrics.models.dao.RubricDao;
 import edu.csula.rubrics.models.dao.UserDao;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
