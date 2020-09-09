cae0004826224b77d16b0df8962a90c35f262470
TracyCYLan <g0170po@gmail.com>
2020-09-09 11:48:44 +0800

    setting oauth2 for spring security

--- src/main/java/edu/csula/rubrics/RubricsApplication.java
+++ src/main/java/edu/csula/rubrics/RubricsApplication.java
@@ -10,10 +10,12 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import edu.csula.rubrics.jwt.filter.JWTAuthorizationFilter;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -38,15 +40,10 @@ public class RubricsApplication {
 	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 		@Override
-		protected void configure(HttpSecurity http) throws Exception {            
-			http.cors().and().csrf().disable()
-					.authorizeRequests()
-					.antMatchers(HttpMethod.GET,"/**").permitAll()
-//					.antMatchers(HttpMethod.POST,"/**").permitAll()
-//					.antMatchers(HttpMethod.PUT,"/**").permitAll()
-//					.antMatchers(HttpMethod.PATCH,"/rubric/**").permitAll()
-//					.antMatchers(HttpMethod.DELETE,"/**").permitAll()
-					.anyRequest().access("@OIDCSecurity.verify(request)");
+		protected void configure(HttpSecurity http) throws Exception {
+			http.cors().and().authorizeRequests()
+			.antMatchers(HttpMethod.GET, "/rubric/**").permitAll()
+			.anyRequest().authenticated().and().oauth2ResourceServer().jwt();
 		}
 
 		// solve the error: has been blocked by CORS policy
@@ -54,10 +51,12 @@ public class RubricsApplication {
 		CorsConfigurationSource corsConfigurationSource() {
 			CorsConfiguration configuration = new CorsConfiguration();
 			configuration.setAllowCredentials(true);
-			configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers","Access-Control-Allow-Origin","Access-Control-Request-Method", "Access-Control-Request-Headers","Origin","Cache-Control", "Content-Type", "Authorization","access_token"));
+			configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+					"Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin", "Cache-Control",
+					"Content-Type", "Authorization"));
 //			configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000","https://alice.cysun.org"));
 			configuration.setAllowedOrigins(Arrays.asList("*"));
-			configuration.setAllowedMethods(Arrays.asList("GET", "POST","PUT","PATCH","DELETE"));
+			configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE"));
 			UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 			source.registerCorsConfiguration("/**", configuration);
 			return source;
